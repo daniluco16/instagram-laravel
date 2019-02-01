ec552422c8e609d34aadbedf0a1389377d623d70
daniluco <daniel-dela@hotmail.com>
2019-02-01 19:29:37 +0100

    1º Creación del proyecto y modelo relacional

---
+++ app/Image.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    protected $table = 'images';
+    
+    //Relacion    One to Many
+    
+    public function comments() {
+        
+        return $this->hasMany('App\Comment');
+        
+    }
+    
+    //Relacion One to Many
+    
+    public function likes() {
+        
+        return $this->hasMany('App\Like');
+        
+    }
+    
+    //Relacion de Muchos a Uno
+    
+    
+    public function users() {
+        
+        return $this->belongsTo('App\User', 'user_id');
+        
+    }
+}
